70322ad3828080d40159d0cd5ed9687b5f8f7765
anavozhko <anastasiia.navozhko@gmail.com>
2017-11-30 21:59:31 +0200

    edit config

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     DEBUG = False
+    TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'nav9862'
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+    # email server
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'your_username@gmail.com'
+    MAIL_PASSWORD = 'password'
+
+    # administrator list
+    ADMINS = ['your_username@gmail.com']
 
 
 class ProductionConfig(Config):
